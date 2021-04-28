8a9c8cd45f2812f0a1764a594f99a04d0f695981
belle scott <bscott@brandeis.edu>
2021-04-28 17:14:36 -0400

    almost done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+    def index
+        @infographs = {"Hurricanes"=> "hurricane info", "Storms"=> "storm info", "Floods" => "flood info"}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#home'
+  root 'home#index'
   #get '/about', to: 
   get '/about/index',   to: 'about#index'##need a controller for about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
