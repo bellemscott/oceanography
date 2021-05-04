545b847a64d6611b29d709aab845e32bcd465b7b
belle scott <bscott@brandeis.edu>
2021-05-03 20:36:33 -0400

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   #get '/about', to: 
-  get '/about/index',   to: 'about#index'##need a controller for about
+  get '/about',   to: 'about#index'##need a controller for about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
